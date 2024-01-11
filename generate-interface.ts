0bd89bb0d2685f2329f536f4efa0719652af2c6d
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-01-12 00:25:26 +0800

    update

--- generate-interface.ts
+++ generate-interface.ts
@@ -1,7 +1,18 @@
 const { convertFromDirectory } =  require('joi-to-typescript');
 
-convertFromDirectory({
-    schemaDirectory: './src/schemas',
-    typeOutputDirectory: './src/interfaces',
-    debug: true
-});
+
+
+import Models from './db/models';
+
+async function run(){
+    await Models.sequelize.sync({ force: true });
+
+
+    convertFromDirectory({
+        schemaDirectory: './src/schemas',
+        typeOutputDirectory: './src/interfaces',
+        debug: true
+    });
+}
+
+run();
