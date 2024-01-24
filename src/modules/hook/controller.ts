001da4f5be75172353191affd84b5e0229737b60
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-01-24 21:49:41 +0800

    update

--- src/modules/hook/controller.ts
+++ src/modules/hook/controller.ts
@@ -65,7 +65,8 @@ export const receiveHook = async (params:{
     shell.cd(find.compose_path);
     executeCmd(shellOpts,find.compose_path,  `docker-compose down`);
     executeCmd(shellOpts,find.compose_path,  `docker rmi $(docker images -qa -f 'dangling=true')`);
-    executeCmd(shellOpts,find.compose_path,  `docker-compose up --build --no-cache --no-deps -d ${query.dep || ''}`);
+    executeCmd(shellOpts,find.compose_path,  `docker-compose build --no-cache ${query.dep || ''}`);
+    executeCmd(shellOpts,find.compose_path,  `docker-compose up --build --force-recreate --no-deps -d ${query.dep || ''}`);
 
 
     return true;
