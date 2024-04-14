282294ad2dae79c1c77f900ec38cc81930a579a2
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-04-14 10:40:05 +0800

    update

--- src/modules/app/index.ts
+++ src/modules/app/index.ts
@@ -2,85 +2,29 @@ import { create, getMany, getOne, remove, update } from "./controller";
 import OpenApiService from "../../services/OpenApiService";
 import ValidateMutationMiddleware from "../../middlewares/ValidateMutationMiddleware";
 
-
 import { AppModelSchema } from "../../schemas";
-const { convert } = require('joi-openapi');
-const express = require('express');
+const { convert } = require("joi-openapi");
+const express = require("express");
 const AppModule = express.Router();
 
+const tags = ["user"];
+const moduleRefName = "user";
 
-const tags = ['user'];
-const moduleRefName = 'user';
-
-AppModule.post('/create',
-    [
-      ValidateMutationMiddleware.body(AppModelSchema({ filter: ['*', '-pk'] })),
-      OpenApiService.path({
-        summary: `create ${moduleRefName}`,
-        tags,
-        responses: {
-          200: {
-            description: 'Successful response',
-            content: {
-              'application/json': {
-                schema: {
-                  type: 'object',
-                  properties: {
-                    status: { type: 'number' },
-                    data: { type: 'object' },
-                  },
-                },
-              },
-            },
-          },
-        },
-        requestBody: {
-          content: {
-            'application/json': {
-              schema: convert(
-                AppModelSchema({ filter: ['*', '-pk'] }).generatedSchema
-              ),
-            },
-          },
-        },
-      }),
-        async (req,res,next)=>{
-            try {
-              console.log(63,req.params.name);
-              console.log(64,req.body);
-                const created = await create(res.locals.input);
-            
-                console.log(11,created);
-
-                res.json({status:1,data:created})
-            } catch(err){
-                next(err);
-            }
-        }
-    ]
-)
-
-
-AppModule.delete(
-  '/delete',
-  ValidateMutationMiddleware.body(
-    AppModelSchema({
-      filter: ['pk'],
-    })
-  ),
+AppModule.post("/create", [
+  ValidateMutationMiddleware.body(AppModelSchema({ filter: ["*", "-pk"] })),
   OpenApiService.path({
-    summary: `delete ${moduleRefName}`,
+    summary: `create ${moduleRefName}`,
     tags,
     responses: {
       200: {
-        description: 'Successful response',
+        description: "Successful response",
         content: {
-          'application/json': {
+          "application/json": {
             schema: {
-              type: 'object',
+              type: "object",
               properties: {
-                status: { type: 'number' },
-                data: { type: 'object' },
+                status: { type: "number" },
+                data: { type: "object" },
               },
             },
           },
@@ -89,8 +33,59 @@ AppModule.delete(
     },
     requestBody: {
       content: {
-        'application/json': {
-          schema: convert(AppModelSchema({ filter: ['pk'] }).generatedSchema),
+        "application/json": {
+          schema: convert(
+            AppModelSchema({ filter: ["*", "-pk"] }).generatedSchema
+          ),
+        },
+      },
+    },
+  }),
+  async (req, res, next) => {
+    try {
+      console.log(63, req.params.name);
+      console.log(64, req.body);
+      const created = await create(res.locals.input);
+
+      console.log(11, created);
+
+      res.json({ status: 1, data: created });
+    } catch (err) {
+      next(err);
+    }
+  },
+]);
+
+AppModule.delete(
+  "/delete",
+  ValidateMutationMiddleware.body(
+    AppModelSchema({
+      filter: ["pk"],
+    })
+  ),
+  OpenApiService.path({
+    summary: `delete ${moduleRefName}`,
+    tags,
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                status: { type: "number" },
+                data: { type: "object" },
+              },
+            },
+          },
+        },
+      },
+    },
+    requestBody: {
+      content: {
+        "application/json": {
+          schema: convert(AppModelSchema({ filter: ["pk"] }).generatedSchema),
         },
       },
     },
@@ -105,21 +100,21 @@ AppModule.delete(
   }
 );
 AppModule.put(
-  '/update',
+  "/update",
   ValidateMutationMiddleware.body(AppModelSchema()),
   OpenApiService.path({
     summary: `update ${moduleRefName}`,
     tags,
     responses: {
       200: {
-        description: 'Successful response',
+        description: "Successful response",
         content: {
-          'application/json': {
+          "application/json": {
             schema: {
-              type: 'object',
+              type: "object",
               properties: {
-                status: { type: 'number' },
-                data: { type: 'object' },
+                status: { type: "number" },
+                data: { type: "object" },
               },
             },
           },
@@ -128,7 +123,7 @@ AppModule.put(
     },
     requestBody: {
       content: {
-        'application/json': {
+        "application/json": {
           schema: convert(AppModelSchema().generatedSchema),
         },
       },
@@ -146,23 +141,21 @@ AppModule.put(
 );
 
 AppModule.get(
-  '/get/:id',
-  ValidateMutationMiddleware.params(
-    AppModelSchema({ filter: [['pk', 'id']] })
-  ),
+  "/get/:id",
+  ValidateMutationMiddleware.params(AppModelSchema({ filter: [["pk", "id"]] })),
   OpenApiService.path({
     summary: `get one ${moduleRefName} by pk`,
     tags,
     responses: {
       200: {
-        description: 'Successful response',
+        description: "Successful response",
         content: {
-          'application/json': {
+          "application/json": {
             schema: {
-              type: 'object',
+              type: "object",
               properties: {
-                status: { type: 'number' },
-                data: { type: 'object' },
+                status: { type: "number" },
+                data: { type: "object" },
               },
             },
           },
@@ -171,13 +164,13 @@ AppModule.get(
     },
     parameters: [
       {
-        in: 'path',
-        name: 'inventory_id',
+        in: "path",
+        name: "inventory_id",
         required: true,
         schema: convert(
-          AppModelSchema({ filter: [['pk', 'id']] }).generatedSchema
+          AppModelSchema({ filter: [["pk", "id"]] }).generatedSchema
         ),
-        description: 'entity primary key',
+        description: "entity primary key",
       },
     ],
   }),
@@ -190,7 +183,7 @@ AppModule.get(
     }
   }
 );
-AppModule.get('/get', async (req, res, next) => {
+AppModule.get("/get", async (req, res, next) => {
   try {
     const many = await getMany();
 
@@ -199,6 +192,5 @@ AppModule.get('/get', async (req, res, next) => {
     next(err);
   }
 });
-
 
 export default AppModule;
--- src/modules/deploy/controller.ts
+++ src/modules/deploy/controller.ts
@@ -6,154 +6,161 @@ import { restructEnv } from "../../utils";
 import { getProvider } from "../hook/utils";
 import { isPortFree, toEnv } from "./utils";
 
-const shell = require('shelljs');
-const path = require('path');
-const fs = require('fs');
-
+const shell = require("shelljs");
+const path = require("path");
+const fs = require("fs");
 
 const config = AppConfig();
 
-console.log(15,config);
+console.log(15, config);
 
-export const create = async (name,data:{
-    env:{
-        NODE_ENV:string
-        PORT:number
-    },
-    branch:string,
-    repo:string,
-    persist:boolean
-})=>{
-    const appName = `${name}-${data.branch}`;
-    const root = path.join(__dirname, `../../../../apps/${appName}`);
-
-    fs.rmSync(root, { recursive: true, force: true });
-    fs.mkdirSync(root, { recursive: true });
-
-    shell.exec(`git stash`);
-    shell.exec(`git stash drop`);
-    const clone = shell.exec(`git clone --branch=${data.branch} ${data.repo} ${root} `);
-    if(clone.code != 0){
-        console.log(`git clone --branch=${data.branch} ${data.repo} ${root} `);
-
-        throw new Error(clone.stderr);
+export const create = async (
+  name,
+  data: {
+    env: {
+      NODE_ENV: string;
+      PORT: number;
     };
+    branch: string;
+    repo: string;
+    persist: boolean;
+  }
+) => {
+  const appName = `${name}-${data.branch}`;
+  const root = path.join(__dirname, `../../../../apps/${appName}`);
 
+  fs.rmSync(root, { recursive: true, force: true });
+  fs.mkdirSync(root, { recursive: true });
 
-    const envs = await persistData(name, root, data);
-    console.log(40,root);
-    console.log(41,envs);
-    shell.cd(root);
-    shell.exec("pwd")
-    shell.exec("ls -alt")
-    const deploy = await shell.exec(`docker compose down  && docker compose up --build -d `,{
-        cwd:root,
-        env:envs
+  shell.exec(`git stash`);
+  shell.exec(`git stash drop`);
+  const clone = shell.exec(
+    `git clone --branch=${data.branch} ${data.repo} ${root} `
+  );
+  if (clone.code != 0) {
+    console.log(`git clone --branch=${data.branch} ${data.repo} ${root} `);
+
+    throw new Error(clone.stderr);
+  }
+
+  const envs = await persistData(name, root, data);
+  console.log(40, root);
+  console.log(41, envs);
+  shell.cd(root);
+  shell.exec("pwd");
+  shell.exec("ls -alt");
+  const deploy = await shell.exec(
+    `docker compose down  && docker compose up --build -d `,
+    {
+      cwd: root,
+      env: envs,
+    }
+  );
+
+  if (deploy.code !== 0) {
+    throw new Error(deploy.stderr);
+  }
+
+  await shell.exec("docker system prune -f");
+
+  return true;
+};
+
+async function persistData(name, root, data) {
+  let app = null;
+  let envs: any = data.env || {};
+  if (data.branch) {
+    app = await Models.AppModel.findOne({
+      raw: true,
+      where: {
+        branch: data.branch,
+        name: name,
+      },
     });
+  }
 
-    if(deploy.code  !== 0){
-        throw new Error(deploy.stderr)
+  if (data.persist) {
+    const provider = getProvider(data.repo) || data.provider;
+
+    if (!app) {
+      const rec: appModelSchema = {
+        branch: data.branch,
+        name,
+        compose_path: root,
+        root_path: root,
+        repo: data.repo,
+        webhook_url: `${config.HOOK_BASE_URL}/${provider}/${name}/${data.branch}`,
+      };
+
+      app = await Models.AppModel.create(rec);
+    } else {
+      const rec: appModelSchema = {
+        branch: data.branch,
+        name,
+        compose_path: root,
+        root_path: root,
+        repo: data.repo,
+        webhook_url: `${config.HOOK_BASE_URL}/${provider}/${name}/${data.branch}`,
+      };
+
+      console.log(103, rec);
+
+      await Models.AppModel.update(rec, {
+        where: {
+          branch: data.branch,
+          name: name,
+        },
+      });
     }
 
-    await shell.exec( "docker system prune -f");
+    if (data.env) {
+      const restruct = Object.keys(data.env).map((key) => {
+        return {
+          prop_key: key,
+          prop_value: data.env[key],
+          app_id: app.app_id,
+        };
+      });
 
-    return true;
+      await LoopService(restruct, async (item, index) => {
+        const find = await Models.EnvModel.findOne({
+          raw: true,
+          where: {
+            prop_key: item.prop_key,
+            app_id: item.app_id,
+          },
+        });
+        console.log(133, find);
+        console.log(134, {
+          prop_key: item.prop_key,
+          app_id: item.app_id,
+        });
 
-}
-
-
-async function persistData(name, root, data){
-    let app = null;
-    let envs: any = data.env || {};
-    if(data.branch){
-        app = await Models.AppModel.findOne({
-            raw:true,
-            where:{
-                branch:data.branch,
-                name:name,
-            }
-        })
-    }
-
-
-    if(data.persist){
-        const provider = getProvider(data.repo) || data.provider;
-
-        if(!app){
-            const rec : appModelSchema= {
-                branch:data.branch,
-                name,
-                compose_path:root,
-                root_path:root,
-                repo:data.repo,
-                webhook_url:`${config.HOOK_BASE_URL}/${provider}/${name}/${data.branch}`
-            }
-
-            app = await Models.AppModel.create(rec);
-        } else {
-            const rec : appModelSchema= {
-                branch:data.branch,
-                name,
-                compose_path:root,
-                root_path:root,
-                repo:data.repo,
-                webhook_url:`${config.HOOK_BASE_URL}/${provider}/${name}/${data.branch}`
-            }
-
-            console.log(103,rec);
-
-            await Models.AppModel.update(rec, {
-                where:{
-                    branch:data.branch,
-                    name:name,
-                }
-            });
+        if (find) {
+          return Promise.resolve({});
         }
 
-        if(data.env){
-            const restruct = Object.keys(data.env).map((key)=>{
-                return {
-                    prop_key:key,
-                    prop_value:data.env[key],
-                    app_id:app.app_id
-                }
-            });
-            await LoopService(restruct,async (item, index)=>{
-                const find = await Models.EnvModel.findOne({
-                    raw:true,
-                    where:item
-                });
-
-                if(find){
-                    return Promise.resolve({})
-                }
-
-                return await Models.EnvModel.create(item);
-            })
-        }
-
+        return await Models.EnvModel.create(item);
+      });
     }
+  }
 
-    if(app && app.app_id){
+  if (app && app.app_id) {
+    const restruct = await restructEnv(app);
 
-
-        const restruct =  await restructEnv(app);
-
-
-        if(restruct){
-            envs = {
-                ...restruct,
-                ...envs,
-            }
-        }
+    if (restruct) {
+      envs = {
+        ...restruct,
+        ...envs,
+      };
     }
+  }
 
-    const content = await toEnv(envs);
+  const content = await toEnv(envs);
 
-    await fs.writeFileSync(`${root}/.env`,content);
+  await fs.writeFileSync(`${root}/.env`, content);
 
-    console.log(135,fs.existsSync(`${root}/.env`))
+  console.log(135, fs.existsSync(`${root}/.env`));
 
-    return envs;
-
+  return envs;
 }
