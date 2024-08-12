e1afdfa74f9c685a69341563578f152b1b4e6013
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-08-12 16:05:12 +0800

    update

--- src/modules/hook/index.ts
+++ src/modules/hook/index.ts
@@ -86,6 +86,8 @@ HookModule.post("/receive/:type/:repo/:current_branch", [
         parse?.branch
       );
 
+      console.log(89, parse);
+
       if (params.current_branch != parse?.branch) {
         throw new Error(
           `pass branch is not the same, ${params.current_branch} != ${parse?.branch}`
